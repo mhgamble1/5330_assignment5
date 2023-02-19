14c7c4cb8f8b91bc4fd24aa7ed4c158c9437faa8
mhgamble1 <mhgamble1@gmail.com>
2023-02-19 10:38:03 -0600

    rename UI elements & link to viewcontroller

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var USD: UITextField!
+    @IBOutlet weak var EUR_switch: UISwitch!
+    @IBOutlet weak var JPY_switch: UISwitch!
+    @IBOutlet weak var GBP_switch: UISwitch!
+    @IBOutlet weak var AUD_switch: UISwitch!
+    @IBOutlet weak var Convert_button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
